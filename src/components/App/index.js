45e2829a3405def3f9fac4e0f740d39f4c88c354
GhislainFaure <ghislainfaure06@gmail.com>
2023-09-05 20:27:41 +0200

    mise en place app react/components/StrictMode

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,20 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import NbColors from "../NbColors";
+import RandomButtons from "../RandomButtons";
+import Colors from "../Colors";
+import Gradient from "../Gradient";
+import DirectionButtons from "../DirectionButtons";
+import "./style.scss";
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-  </div>
-);
-
-// == Export
-export default App;
+export default function App() {
+  return (
+    <div className="app">
+      <NbColors />
+      {/* <RandomButtons />
+      <Colors />
+      <Gradient />
+      <DirectionButtons /> */}
+    </div>
+  );
+}
---
+++ src/components/NbColors/index.js
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+export default function NbColors() {
+  return <div className="nbColors">0 couleur générée</div>;
+}
